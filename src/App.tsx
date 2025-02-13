c6f7b5647f9165318b898a22862d3ad2aafb5d22
Kirill Chuyanov <10977678+archanger@users.noreply.github.com>
2025-02-13 23:00:52 +0200

    test 1 2 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 const App = () => (
   <div style={{ textAlign: "center", padding: "50px" }}>
     <h1>Welcome to My Portfolio 🚀</h1>
-    <p>🤫</p>
+    <p>it works 🤫</p>
   </div>
 );
 
