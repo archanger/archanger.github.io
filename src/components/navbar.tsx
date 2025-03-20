08927154e47e8c09b1f486c16790a2a26b68e158
Kirill Chuyanov <10977678+archanger@users.noreply.github.com>
2025-03-20 22:05:17 +0200

    feat: add linkedin link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,21 @@
 import { ExternalLinkIcon } from 'lucide-react'
+import LinkedIn from '../assets/linkedin.svg?react'
 import ThemeSwitcher from './theme-switcher'
 
 export const Navbar = () => {
   return (
     <nav className='fixed top-6 flex w-full items-center justify-end p-4'>
       <div className='flex items-center space-x-4 rounded-sm p-4 backdrop-blur-lg'>
+        <a
+          href='https://www.linkedin.com/in/kirill-chuyanov/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-primary flex items-center border-b border-transparent p-1 align-middle hover:border-current'
+          aria-label='LinkedIn'
+        >
+          <LinkedIn className='-mt-[2px] size-4' />
+          <ExternalLinkIcon className='-mt-1.5 ml-1' size={16} />
+        </a>
         <a
           href='https://medium.com/@archanger'
           target='_blank'
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    svgr(),
+    //   {
+    //   svgrOptions: {
+    //     exportType: 'default',
+    //     ref: true,
+    //     svgo: false,
+    //     titleProp: true,
+    //   },
+    //   include: '**/*.svg',
+    // }
+  ],
   base: '/',
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 })
