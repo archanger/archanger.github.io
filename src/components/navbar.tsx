b49b8822ffa19f1b087584dac39a6930d61b8481
Kirill Chuyanov <10977678+archanger@users.noreply.github.com>
2025-03-22 16:25:26 +0200

    fix: logo in dark theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
 import { ExternalLinkIcon } from 'lucide-react'
 import LinkedIn from '../assets/linkedin.svg?react'
+import LinkedInLight from '../assets/linkedin_light.svg?react'
 import ThemeSwitcher from './theme-switcher'
+import { useTheme } from './useTheme'
 
 export const Navbar = () => {
+  const { theme } = useTheme()
   return (
     <nav className='sticky top-6 flex w-full items-center justify-end p-4 md:fixed'>
       <div className='flex items-center space-x-4 rounded-sm p-4 backdrop-blur-lg'>
@@ -13,7 +16,11 @@ export const Navbar = () => {
           className='text-primary flex items-center border-b border-transparent p-1 align-middle hover:border-current'
           aria-label='LinkedIn'
         >
-          <LinkedIn className='-mt-[2px] size-4' />
+          {theme === 'dark' ? (
+            <LinkedInLight className='-mt-[2px] size-4' />
+          ) : (
+            <LinkedIn className='-mt-[2px] size-4' />
+          )}
           <ExternalLinkIcon className='-mt-1.5 ml-1' size={16} />
         </a>
         <a
