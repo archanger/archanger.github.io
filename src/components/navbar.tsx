0b1ae982f9c383be6e901741e091f1738b0a311b
Kirill Chuyanov <chuyanov.k@gmail.com>
2025-06-02 15:11:12 +0300

    feat: add link to courses

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,6 +7,16 @@ export const Navbar = () => {
     <nav className='sticky top-6 flex w-full items-center justify-end p-4 md:fixed'>
       <div className='flex items-center space-x-4 rounded-sm p-4 backdrop-blur-lg'>
         <a
+          href='https://codedriven.thinkific.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-primary flex items-center border-b border-transparent align-middle hover:border-current'
+          aria-label='Courses'
+        >
+          Courses
+          <ExternalLinkIcon className='-mt-1.5 ml-1' size={16} />
+        </a>
+        <a
           href='https://www.linkedin.com/in/kirill-chuyanov/'
           target='_blank'
           rel='noopener noreferrer'
